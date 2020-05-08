c6244e21b0a6bf8bc683761d0961cd0d366ed521
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-05-08 21:01:55 +0700

    tugas7js
    mengumpulkan tugas 7 js

---
+++ tugas7.js
@@ -0,0 +1,14 @@
+var tinggiBadan = ["A = 154",
+                   "B = 175",
+                   "C = 180",
+                   "D = 165",
+                   "E = 168",
+                   "F = 155",
+                   "G = 177",
+                   "I = 164",
+                   "J = 173"];
+
+for(let x of tinggiBadan){
+    console.log(x);
+    
+}
